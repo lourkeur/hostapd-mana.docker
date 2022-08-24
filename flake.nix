2337a78f68dc913fd97b2acaa5750f8d6a4da08e
Louis Bettens <louis@bettens.info>
2022-08-24 13:54:33 +0200

    ci: init

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
   inputs.fup.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.1";
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
+  inputs.hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
   inputs.nix2container.url = "github:nlewo/nix2container";
 
   # Outputs are the public-facing interface to the flake.
@@ -20,12 +21,26 @@
 
       supportedSystems = ["x86_64-linux" "aarch64-linux"];
 
+      sharedOverlays = [
+        inputs.hercules-ci-effects.overlay
+      ];
+
       outputsBuilder = channels:
-        with channels.nixpkgs; {
-          packages.default = callPackage nix/package.nix {
-            inherit (inputs.nix2container.packages.${system}) nix2container;
-          };
+      let
+        inherit (channels.nixpkgs) alejandra callPackage effects system;
+        inherit (inputs.nix2container.packages.${system}) nix2container skopeo-nix2container;
+        image = callPackage nix/package.nix { inherit nix2container; };
+      in {
+          packages.default = image;
           formatter = alejandra;
+          effects.publishContainer = effects.mkEffect {
+            inputs = [ image.copyTo skopeo-nix2container ];
+            secretsMap.password = "ghcr-publish-token";
+          effectScript = ''
+            readSecretString password .token | skopeo login ghcr.io -u lourkeur --password-stdin
+            copy-to docker://ghcr.io/lourkeur/hostapd-mana.docker:latest-${system}
+            '';
+          };
         };
     };
 }
