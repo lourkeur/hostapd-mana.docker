377f3f6d1d49953c544219522cedd1f73914dbc9
Louis Bettens <louis@bettens.info>
2023-01-07 15:21:29 +0100

    migrate to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = "hostapd-mana in a container";
 
   # Inputs are how Nix can use code from outside the flake during evaluation.
-  inputs.fup.url = "github:gytis-ivaskevicius/flake-utils-plus/v1.3.1";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
   inputs.hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
@@ -11,36 +11,60 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
 
   # Outputs are the public-facing interface to the flake.
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    fup,
-    ...
-  }:
-    fup.lib.mkFlake {
-      inherit self inputs;
-
-      supportedSystems = ["x86_64-linux" "aarch64-linux"];
-
-      sharedOverlays = [
-        inputs.hercules-ci-effects.overlay
+  outputs = inputs @ {self, ...}:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} ({withSystem, ...}: {
+      imports = [
+        inputs.hercules-ci-effects.flakeModule
       ];
 
-      outputsBuilder = channels: let
-        inherit (channels.nixpkgs) alejandra callPackage effects system;
-        inherit (inputs.nix2container.packages.${system}) nix2container skopeo-nix2container;
-        image = callPackage nix/package.nix {inherit nix2container;};
-      in {
-        packages.default = image;
-        formatter = alejandra;
-        effects.publishContainer = effects.mkEffect {
-          inputs = [image.copyTo skopeo-nix2container];
-          secretsMap.password = "ghcr-publish-token";
-          effectScript = ''
-            readSecretString password .token | skopeo login ghcr.io -u lourkeur --password-stdin
-            copy-to docker://ghcr.io/lourkeur/hostapd-mana.docker:latest-${system}
-          '';
+      systems = ["x86_64-linux" "aarch64-linux"];
+
+      perSystem = {
+        self',
+        inputs',
+        pkgs,
+        ...
+      }: {
+        packages.default = pkgs.callPackage nix/package.nix {
+          inherit (inputs'.nix2container.packages) nix2container;
         };
+        formatter = pkgs.alejandra;
       };
-    };
+
+      flake.effects = withSystem "x86_64-linux" (
+        {
+          self',
+          inputs',
+          pkgs,
+          hci-effects,
+          system,
+          ...
+        }: let
+          inherit (inputs'.nix2container.packages) skopeo-nix2container;
+        in {
+          publishContainer.x86_64 = let
+            image = self'.packages.default;
+          in
+            hci-effects.mkEffect {
+              inputs = [skopeo-nix2container];
+              secretsMap.password = "ghcr-publish-token";
+              effectScript = ''
+                readSecretString password .token | skopeo login ghcr.io -u lourkeur --password-stdin
+                skopeo --insecure-policy copy nix:${image} docker://ghcr.io/lourkeur/hostapd-mana.docker:latest-${system}
+              '';
+            };
+          publishContainer.aarch64 = let
+            image = self.packages.aarch64-linux.default;
+          in
+            hci-effects.mkEffect {
+              inputs = [skopeo-nix2container];
+              secretsMap.password = "ghcr-publish-token";
+              effectScript = ''
+                readSecretString password .token | skopeo login ghcr.io -u lourkeur --password-stdin
+                skopeo --insecure-policy copy nix:${image} docker://ghcr.io/lourkeur/hostapd-mana.docker:latest-aarch64-linux
+              '';
+            };
+        }
+      );
+    });
 }
